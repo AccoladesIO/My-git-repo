96f80735f47c0612563c56f5bc71390f51137be2
unknown <ononobip@gmail.com>
2023-01-04 16:43:24 +0100

    update: added a few custom hooks

---
+++ src/hooks/usePaginate.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { useFetchRepo } from "./useFetchRepo";
+
+export const usePaginate = () => {
+    const {repo} = useFetchRepo()
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [postPerPage, setPostPerPage] = useState(5)
+ 
+
+  const lastPostIndex = currentPage * postPerPage;
+  const firstPostIndex = lastPostIndex - postPerPage
+  const currentRepo = repo.slice(firstPostIndex, lastPostIndex)
+
+
+  return {setCurrentPage, currentRepo, postPerPage, currentPage}
+} 
