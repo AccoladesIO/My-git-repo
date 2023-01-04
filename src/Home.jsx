bb7d8f9aa7750cb66d7a4b2132fe7350295c6c4f
unknown <ononobip@gmail.com>
2023-01-04 16:22:55 +0100

    update: Home component update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,40 +1,16 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
 import RepoList from './components/RepoList'
 import Lists from './components/Lists'
 import Loading from './components/Loading'
 import Navbar from './components/Navbar'
 import Users from './components/Users'
 import Footer from './components/Footer'
+import { useFetch } from './hooks/useFetch'
 
 const Home = () => {
 
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [postPerPage, setPostPerPage] = useState(8)
-
-  const url = 'https://api.github.com/users/accoladesio';
-
-  const fetchData = async () => {
-    // enable loading state while fetching data
-    setLoading(true)
-
-    // waiting for response from Api Call
-    const response = await fetch(url);
-    const result = await response.json();
-    console.log(result);
-
-    // disable loading state once data is fetched
-    setLoading(false)
-    setData(result)
-    console.log(data)
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
+  const {data, loading} = useFetch()
+  
   if(loading){
     return(
       <main>
